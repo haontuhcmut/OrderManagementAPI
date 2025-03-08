f7a19daff8534217901c3dfe1130bad4f2cf6c6b
haontuhcmut <nguyenminhhao1188@gmail.com>
2025-03-08 11:30:52 +0700

    tiny edited

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -6,6 +6,7 @@ from fastapi.security import OAuth2PasswordBearer
 from app.db.redis import token_in_blocklist
 from app.auth.utils import decode_token
 
+
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
